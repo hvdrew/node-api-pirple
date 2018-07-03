29d2f2a1d5ce028cb1a7314425187b34788ca0b6
Andrew <andrew.merison@gmail.com>
2018-07-03 08:22:48 -0700

    Added config file and setup server to use it properly.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -75,8 +76,8 @@ const server = http.createServer((req, res) => {
 });
 
 // Start the server, and have it listen on port 3000
-server.listen(3000, () => {
-    console.log('The server is listening on port 3000 now');
+server.listen(config.port, () => {
+    console.log(`The server is listening on port ${config.port} in ${config.envName} mode.`);
 });
 
 // Define the handlers
